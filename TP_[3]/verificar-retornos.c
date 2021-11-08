296419d7b85b04a37a0244e2c7423ab00e14d0b4
Matias Ferreira <ferre.matias@hotmail.com>
2021-11-08 12:15:32 -0300

    tp 3 v1

---
+++ TP_[3]/verificar-retornos.c
@@ -0,0 +1,81 @@
+#include "verificar-retornos.h"
+
+void retornos_loadFromAny(int estado, int *retorno) {
+
+	*retorno = estado;
+	if (estado == 0) {
+		printf ("\nLa lista se cargo correctamente.");
+	} else {
+		printf ("\nError en la carga.");
+	}
+}
+
+void retornos_addEmployee (int estado, int *retorno) {
+	*retorno = estado;
+
+	if (estado == 0) {
+		printf ("\nEl empleado se agrego correctamente.");
+	} else if (estado == -1) {
+		printf ("\nError, al agregar al empleado.");
+	}else if (estado == -2) {
+		printf ("\nError, ingrese muchas veces opciones invalidas, reintente luego.");
+	} else {
+		printf ("\nError en la lista.");
+	}
+}
+
+void retornos_editEmployee (int estado) {
+
+	if (estado == 0) {
+		printf ("\nCliente modificado con exito.");
+	}else if (estado == -1) {
+		printf ("\nError en modificacion del empleado.");
+	} else if (estado == -2) {
+		printf ("\nError, ingreso muchas veces una opcion incorrecta. Reintente luego.");
+	} else {
+		printf ("\nError en la lista.");
+	}
+
+}
+
+void retornos_removeEmployee (int estado) {
+	if (estado == 0) {
+		printf ("\nCliente eliminado con exito.");
+	} else if (estado == -1) {
+		printf ("\nBaja cancelada.");
+	} else if (estado == -2){
+		printf ("\nError, ingreso muchas veces una opcion incorrecta. Reintente luego.");
+	} else {
+		printf ("\nError en la lista.");
+	}
+}
+
+void retornos_ListEmployee (int estado) {
+	if (estado == -1) {
+		printf ("\nLa lista esta vacia.");
+	} else if (estado == -2) {
+		printf ("\nError en la lista.");
+	}
+}
+
+void retornos_sortEmployee (int estado) {
+	if (estado == 0) {
+		printf ("\nLa lista se a ordenado exitosamente.");
+	} else if (estado == -1) {
+		printf ("\nError al realizar el ordenamiento.");
+	} else if (estado == -2) {
+		printf ("\nError, ingreso muchas veces una opcion incorrecta. Reintente luego.");
+	} else {
+		printf ("\nError en la lista.");
+	}
+}
+
+void retornos_saveAsAny (int estado) {
+	if (estado == 0) {
+		printf ("\nLista guardada exitosamente.");
+	} else if (estado == -1) {
+		printf ("\nError al abrir el archivo para guardar.");
+	} else {
+		printf ("\nError en el directorio/lista");
+	}
+}
---
+++ TP_[3]/verificar-retornos.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#ifndef VERIFICAR_RETORNOS_H_
+#define VERIFICAR_RETORNOS_H_
+
+void retornos_loadFromAny(int estado, int *retorno);
+void retornos_addEmployee (int estado, int *retorno);
+void retornos_editEmployee (int estado);
+void retornos_removeEmployee (int estado);
+void retornos_ListEmployee (int estado);
+void retornos_sortEmployee (int estado);
+void retornos_saveAsAny (int estado);
+
+#endif /* VERIFICAR_RETORNOS_H_ */
